46dfd687c827b2f44e64814b5a865c7ebbcf49c8
Chris Hanna <channa@aptima.com>
2016-06-29 19:59:24 -0400

    init

---
+++ DCG3/IPrimitiveBatch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace DCG3
+{
+    interface IPrimitiveBatch
+    {
+        void Begin();
+        void Draw(Vector3 position, float width, float height, float depth, Color color);
+        void Flush(Matrix viewMatrix);
+    }
+}
