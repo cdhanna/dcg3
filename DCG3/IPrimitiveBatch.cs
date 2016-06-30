ff4189f4ee40cfd7f811cbd82ec802dbc4a7fffb
Chris Hanna <channa@aptima.com>
2016-06-29 20:03:00 -0400

    adds graphicsdevice to init

--- DCG3/IPrimitiveBatch.cs
+++ DCG3/IPrimitiveBatch.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace DCG3
 {
     interface IPrimitiveBatch
     {
-        void Begin();
+        void Begin(GraphicsDevice device);
         void Draw(Vector3 position, float width, float height, float depth, Color color);
         void Flush(Matrix viewMatrix);
     }
--- DCG3/Program.cs
+++ DCG3/Program.cs
@@ -14,7 +14,7 @@ namespace DCG3
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new TestGame())
                 game.Run();
         }
     }
