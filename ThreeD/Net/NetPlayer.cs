cbea602e8beb468285e00d388fd8ae7d4c336b52
Greg Daneault <gregdaneault@gmail.com>
2016-09-04 16:19:31 -0400

    Added basic server

---
+++ ThreeD/Net/NetPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCG.Framework.Net
+{
+    class NetPlayer
+    {
+        private static int nextId = 0;
+        public NetPlayer(TcpClient tcpClient)
+        {
+            this.Socket = tcpClient;
+            this.Id = nextId++;
+        }
+
+        public int Id { get; private set; }
+        public TcpClient Socket { get; private set; }
+    }
+}
